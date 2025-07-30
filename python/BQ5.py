dac079ed7b260665d54b4c10c9202438d039e980
Malavika T R <malavikatr29@gmail.com>
2025-07-30 15:47:20 +0530

    5 and 6

---
+++ python/BQ5.py
@@ -0,0 +1,7 @@
+n = input()
+#using built-in function
+n1 = ''.join(reversed(n))
+print(n1)
+
+#not using built-in function
+# print(n[::-1])
---
+++ python/BQ6.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = 0
+b = 1
+c = 0
+for i in range(n):
+    c = a
+    print(a,end=" ")
+    a = b           #a,b = b, a+b
+    b = c + b
